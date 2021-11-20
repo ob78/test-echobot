5742d4f967138fc7282afa77bb201cfb6ab8036f
ob78 <61279321+ob78@users.noreply.github.com>
2021-11-20 23:23:30 +0100

    Update app.py

--- app.py
+++ app.py
@@ -77,12 +77,15 @@ async def messages(req: Request) -> Response:
         return json_response(data=response.body, status=response.status)
     return Response(status=HTTPStatus.OK)
 
-
-APP = web.Application(middlewares=[aiohttp_error_middleware])
-APP.router.add_post("/api/messages", messages)
-
+def init_func(argv):
+    APP = web.Application(middlewares=[aiohttp_error_middleware])
+    APP.router.add_post("/api/messages", messages)
+    return APP
+    
 if __name__ == "__main__":
+    APP = init_func(None)
     try:
-        web.run_app(APP, host="https://flyme-webapp.azurewebsites.net", port=CONFIG.PORT)
+        #web.run_app(APP, host="https://flyme-webapp.azurewebsites.net", port=CONFIG.PORT)
+        web.run_app(APP, host="0.0.0.0", port=CONFIG.PORT)
     except Exception as error:
         raise error
